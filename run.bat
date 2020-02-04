bc0cf0b67afb3c9ab388b30ab6f042a63d873dd2
Sergey Bobysh <serb@tut.by>
2020-02-04 22:26:52 +0300

    Extract a rar file

--- run.bat
+++ run.bat
@@ -1,3 +1,4 @@
 echo off
-echo Runner the docker alpine example
-docker run -it alpine:3.7 /bin/sh
+echo Extract a rar file
+REM docker run --rm -v %CD%:/files maxcnunes/unrar x -r Trunk.rar
+docker run --rm -v %CD%:/files alpine:3.7 cat /files/text
