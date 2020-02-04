0a9ff3f4af161fcc3fa078d21bc59a7719392504
Sergey Bobysh <serb@tut.by>
2020-02-04 22:56:31 +0300

    Configured ports binding

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 echo off
 echo NodeJS web server
-docker run -it --rm --name node -d -v %CD%:/src -w /src node:12.14.1-alpine3.9 node app.js
+docker run -it --rm --name node -d -v %CD%:/src -w /src -p 8080:3000 node:12.14.1-alpine3.9 node app.js
